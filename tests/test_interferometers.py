ea09551057dd3ae21fc3cc92a06ee77126d06f42
olgasolodovnikova <osolodovnikova@hotmail.com>
2025-08-11 11:07:18 +0200

    cleaning up the repo

--- tests/test_interferometers.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-
-    from bosonicplus.interferometers.parameters import gen_interferometer_params
-    params = gen_interferometer_params(10, 12)
-    params = gen_interferometer_params(10, 12, bs_arrange='cascade')
-    params = gen_interferometer_params(10, 12, bs_arrange='inv_cascade')
-
-    from bosonicplus.interferometers.construct import build_interferometer
-    circuit = build_interferometer(params, 10, True)
